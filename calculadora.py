f0b710dc56a91315ab8387492dbdad4310e97762
Valéria Galdino Ferreira <vgaldino01@gmail.com>
2026-07-13 19:31:39 -0300

    Meu primeiro projeto Python

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+print("Minha primeira calculadora no GitHub")
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+
+soma = numero1 + numero2
+
+print("Resultado:", soma)
